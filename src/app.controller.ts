938dcd2e40bdf6d87c3cfc088a354a0f69180631
youngkim <youngkim9012@gmail.com>
2022-07-20 00:44:13 +0900

    config 대체

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
-    return process.env.DATABASE_HOST;
+    return this.configService.get('DATABASE_HOST');
   }
 }
