85328612f2a1f8dc8331355883897d60fa3d3d84
youngkim <youngkim9012@gmail.com>
2022-07-21 22:58:34 +0900

    class-transform 유효성 검사 데코레이터 적용

--- src/common/validation.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
-
-@Injectable()
-export class ValidationPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
-    console.log(metadata);
-    return value;
-  }
-}
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,11 +1,33 @@
-import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { Transform } from 'class-transformer';
+import { BadRequestException } from '@nestjs/common';
 
 export class CreateUserDto {
+  @Transform(({ value, obj }) => {
+    if (obj.password.includes(value.trim())) {
+      throw new BadRequestException(
+        'password는 name과 같은 문자열을 포함할 수 없습니다.',
+      );
+    }
+    return value.trim();
+  })
   @IsString()
-  @MinLength(1)
-  @MaxLength(20)
-  name: string;
+  @MinLength(2)
+  @MaxLength(30)
+  readonly name: string;
 
+  @IsString()
   @IsEmail()
-  email: string;
+  @MaxLength(60)
+  readonly email: string;
+
+  @IsString()
+  @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/) // 정규표현식으로 문자열 규칙 적용
+  readonly password: string;
 }
